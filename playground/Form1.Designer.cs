09ba8b19ef0d338a7188b00a97285cd99abca005
Abdallahreda97 <82894407+Abdallahreda97@users.noreply.github.com>
2021-05-31 16:14:07 +0300

    log in 1

--- playground/Form1.Designer.cs
+++ playground/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace playground
 {
-    partial class Form1
+    partial class formlogin
     {
         /// <summary>
         /// Required designer variable.
@@ -29,46 +29,112 @@ namespace playground
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.login = new System.Windows.Forms.Button();
+            this.signup = new System.Windows.Forms.Button();
+            this.UserName = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // Exit
             // 
-            this.button1.Location = new System.Drawing.Point(484, 297);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(154, 77);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Exit";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Exit.BackColor = System.Drawing.Color.Red;
+            this.Exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Exit.Location = new System.Drawing.Point(247, 143);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(123, 68);
+            this.Exit.TabIndex = 0;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // login
             // 
-            this.button2.Location = new System.Drawing.Point(29, 297);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(405, 70);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "log in";
-            this.button2.UseVisualStyleBackColor = true;
+            this.login.BackColor = System.Drawing.Color.Lime;
+            this.login.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.login.Location = new System.Drawing.Point(12, 143);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(229, 68);
+            this.login.TabIndex = 1;
+            this.login.Text = "log in";
+            this.login.UseVisualStyleBackColor = false;
+            this.login.Click += new System.EventHandler(this.login_Click);
             // 
-            // Form1
+            // signup
+            // 
+            this.signup.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.signup.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.signup.Location = new System.Drawing.Point(287, 12);
+            this.signup.Name = "signup";
+            this.signup.Size = new System.Drawing.Size(83, 25);
+            this.signup.TabIndex = 2;
+            this.signup.Text = "sign up";
+            this.signup.UseVisualStyleBackColor = false;
+            // 
+            // UserName
+            // 
+            this.UserName.AutoSize = true;
+            this.UserName.Location = new System.Drawing.Point(31, 61);
+            this.UserName.Name = "UserName";
+            this.UserName.Size = new System.Drawing.Size(59, 13);
+            this.UserName.TabIndex = 3;
+            this.UserName.Text = "User Name";
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Location = new System.Drawing.Point(31, 105);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(53, 13);
+            this.Password.TabIndex = 4;
+            this.Password.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 105);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(174, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // formlogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(384, 220);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.UserName);
+            this.Controls.Add(this.signup);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.Exit);
+            this.Name = "formlogin";
             this.Text = "log in";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.Button signup;
+        private System.Windows.Forms.Label UserName;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- playground/Form1.cs
+++ playground/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace playground
 {
-    public partial class Form1 : Form
+    public partial class formlogin : Form
     {
-        public Form1()
+        public formlogin()
         {
             InitializeComponent();
         }
@@ -20,6 +20,26 @@ namespace playground
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            string name, password;
+            name = textBox1.Text;
+            password = textBox2.Text;
+            if (name == "abc" && password == "123")
+            {
+                MessageBox.Show("تم تسجيل الدخول بنجاح");
+            }
+            else
+            {
+                MessageBox.Show(" حاول مرة اخرى");
+            }
         }
     }
 }
