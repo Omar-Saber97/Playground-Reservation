61550be08520a5497a88b45df64be99a4b413c22
Ammar Mohamed <amarkingstone20@gmail.com>
2021-05-31 23:07:33 +0200

    remove button

--- playground/signup.Designer.cs
+++ playground/signup.Designer.cs
@@ -29,24 +29,14 @@ namespace playground
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(444, 165);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // signup
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(933, 554);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "signup";
             this.Text = "signup";
             this.ResumeLayout(false);
@@ -54,7 +44,5 @@ namespace playground
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
